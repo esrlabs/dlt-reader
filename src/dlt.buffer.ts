2c2aa288622c739e8ba04c4b6577475df81c2483
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-04-03 11:43:18 +0200

    init commit

---
+++ src/dlt.buffer.ts
@@ -0,0 +1,37 @@
+import { EventEmitter } from 'events';
+import { Buffer } from 'buffer';
+import Packet, { IPacketData } from './dlt.packet';
+
+export default class DLTBuffer extends EventEmitter {
+
+    public static Events = {
+        packet: 'packet',
+    };
+
+    private _buffer: Buffer = new Buffer(0);
+
+    constructor() {
+        super();
+    }
+
+    public add(buffer: Buffer) {
+        this._buffer = Buffer.concat([this._buffer, buffer]);
+        do {
+            const status: boolean = this._read();
+        } while (status);
+    }
+
+    private _read(): boolean {
+        const processor: Packet = new Packet(this._buffer);
+        const packet: IPacketData | Error = processor.read();
+        if (packet instanceof Error) {
+            return false;
+        }
+        // Remove already read message from buffer
+        this._buffer = processor.crop();
+        // Trigger event
+        this.emit(DLTBuffer.Events.packet, packet);
+        return true;
+    }
+
+}
